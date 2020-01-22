3c8ad2d4ed55bb18d0789a8f4aca7a5e73aa2712
casparhendrik <casparhendrik@hotmail.nl>
2020-01-22 10:26:13 +0100

    Fix of search

--- src/app/activities-page/activities-page.module.ts
+++ src/app/activities-page/activities-page.module.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ActivitiesPagePage } from './activities-page.page';
-import { ActivitiesScrollviewComponent } from '../activities-scrollview/activities-scrollview.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
